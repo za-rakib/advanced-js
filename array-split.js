ffe537517abf2746af7370226ead48545697230f
Rakib Hassan <phpzarakib@gmail.com>
2021-02-06 16:35:08 +0600

    commit

---
+++ array-split.js
@@ -0,0 +1,9 @@
+const number =[0,1,2,3,4,5,6,7,8,9];
+ //const part = number.slice(2,5);
+ //const removed = number.splice(2,5);
+ const removed = number.splice(2,5,70,80);
+ const together = number.join(" ");
+ //console.log(part);
+//  console.log(removed);
+//  console.log(number);
+console.log(together);
---
+++ break-con.js
@@ -0,0 +1,7 @@
+const number =[0,1,2,3,4,5,6,7,8,9];
+for (let i = 0; i < number.length; i++) {
+    if(number[i]>4){
+        break;
+    }
+    console.log(number[i]);
+}
